44db75f6f67949161f465dd0408be24c6b02b6aa
Minter Face <ministerface@gmail.com>
2016-08-20 00:37:32 +0300

    add excel.png

--- app/app.component.js
+++ app/app.component.js
@@ -12,6 +12,7 @@ var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Wholesale access interface';
+        this.copyright = 'DonRiver';
     }
     AppComponent = __decorate([
         core_1.Component({
